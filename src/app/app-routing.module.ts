fee2fe4c9c141384001c829601714426f587e0ed
Daniel Matos <deme12.1@gmail.com>
2023-03-30 22:45:02 -0500

    Páginas generadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MesasComponent } from './pages/mesas/mesas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { PedidoComponent } from './pages/pedido/pedido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'mesas', component: MesasComponent},
+  {path:'productos', component: ProductosComponent},
+  {path:'pedidos', component: PedidoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MesasComponent } from './pages/mesas/mesas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { PedidoComponent } from './pages/pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MesasComponent,
+    ProductosComponent,
+    PedidoComponent
   ],
   imports: [
     BrowserModule,
