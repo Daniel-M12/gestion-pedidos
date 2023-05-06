2739f91dc5bde3e8c11345bf33bfe9e053721bc2
Daniel Matos <deme12.1@gmail.com>
2023-05-06 08:35:46 -0500

    Se puede agregar productos

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Producto } from 'src/app/models/producto.model';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
+  constructor(
+    private fb: FormBuilder,
+    private readonly ps: ProductosService
+  ){}
 
+  productosForm = this.fb.group({
+    nombre: ['', Validators.required],
+    precio: [0.0, Validators.required],
+    imagen: ['', Validators.required],
+    categoria: [0, Validators.required],
+    descripcion: ''
+  });
+
+  agregarProducto(producto: any){
+    const token = sessionStorage.getItem('token');
+    const header = { Authorization: 'Bearer ' + token };
+
+    this.ps.insert_product(producto, header).subscribe((rest:any)=>{
+      if(rest.isSuccess){
+        console.log(rest);
+        alert('Producto creado con ID: ' + rest.data.id);
+        
+      }else{
+        alert(rest.errorMessage)
+      }
+    })
+  }
+
+  onSubmit(){
+    if(this.productosForm.valid){
+      console.log(this.productosForm.value);
+      this.agregarProducto(this.productosForm.value);
+    }
+  }
 }
